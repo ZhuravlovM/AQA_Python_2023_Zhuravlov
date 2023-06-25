372cd28990352abb355a6aeee83f8561672a5f45
ZhuravlovM <zhuravlov.mykyta@gmail.com>
2023-06-25 15:47:08 +0300

    Update Homework2

---
+++ Lesson2_DataTypes/Homework2/Task1_NameSurname/name_surname_output.py
@@ -0,0 +1,15 @@
+#Subtask1
+first_name = "Mykyta"
+last_name = "Zhuravlov"
+
+full_name = first_name + " " + last_name
+print("My full name is " + full_name)
+
+print("Lowercase: " + full_name.lower())
+print("Uppercase: " + full_name.upper())
+
+#Subtask2
+lowercase_name = full_name.lower()
+print("Capital letters: "+ lowercase_name.title())
+
+#Subtask3
---
+++ Lesson2_DataTypes/Homework2/Task2_CurrencyRate/currency_rate.py
@@ -0,0 +1,5 @@
+usd_rate = 37.25
+uah_available = 1000
+uah_to_usd = round((uah_available / usd_rate), 2)
+
+print("Поточний курс складає: {}".format(uah_to_usd))
