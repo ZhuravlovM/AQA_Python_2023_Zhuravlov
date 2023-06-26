3c10b57303bec1a310a56c2c82cc45922fc2b0b7
ZhuravlovM <zhuravlov.mykyta@gmail.com>
2023-06-26 14:23:49 +0300

    Update name_surname_output.py

--- Lesson2_DataTypes/Homework2/Task1_NameSurname/name_surname_output.py
+++ Lesson2_DataTypes/Homework2/Task1_NameSurname/name_surname_output.py
@@ -14,7 +14,7 @@ print("Capital letters: " + lowercase_name.title())
 
 # Subtask3
 name_with_spaces = "\n\t" + full_name + "\t"
-print(name_with_spaces + "is my name\n")
+print(name_with_spaces + "is my name with spaces\n")
 print(name_with_spaces.strip() + " - is printed with strip")
 print(name_with_spaces.lstrip() + " - is printed with lstrip")
 print(name_with_spaces.rstrip() + " - is printed with rstrip")
