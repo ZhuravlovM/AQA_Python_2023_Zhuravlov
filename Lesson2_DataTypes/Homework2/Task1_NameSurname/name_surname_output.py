dc6ef1cda09e31666dd44589c54bc763ea0708d2
ZhuravlovM <zhuravlov.mykyta@gmail.com>
2023-06-26 14:21:50 +0300

    Update Homework2

--- Lesson2_DataTypes/Homework2/Task1_NameSurname/name_surname_output.py
+++ Lesson2_DataTypes/Homework2/Task1_NameSurname/name_surname_output.py
@@ -1,4 +1,4 @@
-#Subtask1
+# Subtask1
 first_name = "Mykyta"
 last_name = "Zhuravlov"
 
@@ -8,8 +8,14 @@ print("My full name is " + full_name)
 print("Lowercase: " + full_name.lower())
 print("Uppercase: " + full_name.upper())
 
-#Subtask2
+# Subtask2
 lowercase_name = full_name.lower()
-print("Capital letters: "+ lowercase_name.title())
+print("Capital letters: " + lowercase_name.title())
 
-#Subtask3
+# Subtask3
+name_with_spaces = "\n\t" + full_name + "\t"
+print(name_with_spaces + "is my name\n")
+print(name_with_spaces.strip() + " - is printed with strip")
+print(name_with_spaces.lstrip() + " - is printed with lstrip")
+print(name_with_spaces.rstrip() + " - is printed with rstrip")
+print(name_with_spaces.replace("\t","").replace("\n","").replace(" ","") + " - is printed with replace")
