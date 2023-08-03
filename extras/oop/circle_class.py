794f86e4cfe46b622b3ea0ebec7a0985fbbb728a
ZhuravlovM <zhuravlov.mykyta@gmail.com>
2023-08-03 17:24:44 +0300

    extras updated

---
+++ extras/oop/circle_class.py
@@ -0,0 +1,20 @@
+import math
+
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+
+    def calc_area(self):
+        area = math.pi * self.radius ** 2
+        print("Circle area is:", area)
+
+    def calc_perimeter(self):
+        perimeter = 2 * math.pi * self.radius
+        print("Circle perimeter is:", perimeter)
+
+
+circle = Circle(5)
+
+circle.calc_area()
+circle.calc_perimeter()
