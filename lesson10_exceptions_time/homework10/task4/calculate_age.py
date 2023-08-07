a1ba9991b6a7271ac17edcc2dc3e935f9fdf5463
ZhuravlovM <zhuravlov.mykyta@gmail.com>
2023-08-07 15:57:22 +0300

    Minor change in current_age, line6

--- lesson10_exceptions_time/homework10/task4/calculate_age.py
+++ lesson10_exceptions_time/homework10/task4/calculate_age.py
@@ -3,12 +3,12 @@ import datetime
 
 def age_calculation(birth_date):
     current_date = datetime.datetime.now()
-    current_age = current_date - birth_date
+    current_age = round((current_date - birth_date).days/365)
     age_timestamp = birth_date.timestamp()
     return current_age, age_timestamp
 
 
-birthday = datetime.datetime(year=1997, month=5, day=20, hour=14, minute=15, second=00)
+birthday = datetime.datetime(year=1998, month=5, day=20, hour=14, minute=15, second=00)
 age, timestamp = age_calculation(birthday)
 print("Точний вік:", age)
 print("Timestamp:", timestamp)
