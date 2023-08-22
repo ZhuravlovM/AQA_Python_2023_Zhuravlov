79480d3bad47024e8102332a32d8e506c4ddcf2c
ZhuravlovM <zhuravlov.mykyta@gmail.com>
2023-08-22 17:10:07 +0300

    homework17 added

--- lesson17_adapter_proxy_pytest/homework17/task2/csv_class_test/test_csv.py
+++ lesson17_adapter_proxy_pytest/homework17/task2/csv_class_test/test_csv.py
@@ -7,34 +7,22 @@ class TestCSV:
     def setup_class(self):
         self.csv_file = "example.csv"
 
-    def test_add_row(self):
-        manipulator = CSVController(self.csv_file)
-        test_row = ["TestFirstName", "TestLastName", "25", "Male", "5000"]
+    def test_add_row_to_csv(self):
+        controller = CSVController(self.csv_file)
+        new_row = ['John', 'Doe', '25', 'Male', '5000']
+        controller.add_row(new_row)
 
-        manipulator.add_row(test_row)
+        with open(self.csv_file, 'r') as csvfile:
+            csv_reader = csv.reader(csvfile)
+            rows = list(csv_reader)
+            assert new_row in rows
 
-        with open(self.csv_file, 'r') as file:
-            reader = csv.reader(file)
-            rows = list(reader)
-            assert test_row in rows
+    def test_delete_row_from_csv(self):
+        controller = CSVController(self.csv_file)
+        row_index_to_delete = 1
+        controller.delete_row(row_index_to_delete)
 
-    def test_delete_row(self):
-        handler = CSVController(self.csv_file)
-
-        initial = [
-            ["Elizabet", "Fork", "19", "Female", "3000"],
-            ["Reginald", "Lidoo", "42", "Male", "2500"],
-            ["Alexander", "Great", "30", "Male", "15000"]
-        ]
-        with open(self.csv_file, 'w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerows(initial)
-
-        row_to_delete = 1
-        handler.delete_row(row_to_delete)
-
-        with open(self.csv_file, 'r') as file:
-            reader = csv.reader(file)
-            remaining_rows = list(reader)
-            assert len(remaining_rows) == len(initial) - 1
-            assert remaining_rows[row_to_delete] != ["Reginald", "Lidoo", "42", "Male", "2500"]
+        with open(self.csv_file, 'r') as csvfile:
+            csv_reader = csv.reader(csvfile)
+            rows = list(csv_reader)
+            assert len(rows) == 3
--- lesson17_adapter_proxy_pytest/homework17/task2/csv_func_for_testing.py
+++ lesson17_adapter_proxy_pytest/homework17/task2/csv_func_for_testing.py
@@ -2,24 +2,24 @@ import csv
 
 
 class CSVController:
-    def __init__(self, csv_file):
-        self.csv_file = csv_file
+
+    def __init__(self, file_path):
+        self.file_path = file_path
 
     def add_row(self, row_data):
-        with open(self.csv_file, 'a', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(row_data)
+        with open(self.file_path, 'a', newline='') as csvfile:
+            csv_writer = csv.writer(csvfile)
+            csv_writer.writerow(row_data)
 
     def delete_row(self, row_index):
         rows = []
-        with open(self.csv_file, 'r') as file:
-            reader = csv.reader(file)
-            for row in reader:
+        with open(self.file_path, 'r') as csvfile:
+            csv_reader = csv.reader(csvfile)
+            for row in csv_reader:
                 rows.append(row)
 
-        if 0 <= row_index < len(rows):
-            rows.pop(row_index)
-
-        with open(self.csv_file, 'w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerows(rows)
+        with open(self.file_path, 'w', newline='') as csvfile:
+            csv_writer = csv.writer(csvfile)
+            for index, row in enumerate(rows):
+                if index != row_index:
+                    csv_writer.writerow(row)
