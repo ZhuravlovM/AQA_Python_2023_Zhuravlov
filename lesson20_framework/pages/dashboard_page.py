eec4c7245bd3bad8dc613ec6b28ddcbf0cc90017
ZhuravlovM <zhuravlov.mykyta@gmail.com>
2023-08-31 13:05:51 +0300

    lesson20 added

---
+++ lesson20_framework/pages/dashboard_page.py
@@ -0,0 +1,16 @@
+from lesson20_framework.pages.base_page import BasePage
+
+
+class Dashboard(BasePage):
+    def __init__(self, driver):
+        super().__init__(driver)
+
+    def go_to_comix_and_books(self):
+        locator = ('xpath', '//div[@class="products-menu__title"]/a[@href="/komiksy-i-knigi/"]')
+        element = self.wait_until_element_appears(locator)
+        element.click()
+
+    def go_to_login_form(self):
+        locator = ('xpath', '//a[@title="Вхід"]')
+        element = self.wait_until_element_appears(locator)
+        element.click()
