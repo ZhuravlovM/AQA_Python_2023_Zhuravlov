adcac1d51ed4cb9e34d3f90b1834d3e2f9a49e25
ZhuravlovM <zhuravlov.mykyta@gmail.com>
2023-09-16 15:36:00 +0300

    lesson25 updated

--- lesson25_resful_api/conftest.py
+++ lesson25_resful_api/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+import lesson25_resful_api.infrastructure
 
 
 @pytest.fixture()
--- lesson25_resful_api/infrastructure.py
+++ lesson25_resful_api/infrastructure.py
@@ -1,5 +1,7 @@
 import requests
 import json
+from requests.auth import HTTPBasicAuth
+from requests.exceptions import HTTPError
 
 url = "https://api.restful-api.dev/objects"
 
@@ -10,13 +12,35 @@ def get_an_object(object_id):
 
 
 def create_an_object():
-    headers = {'content-type': 'application/json'}
+    headers = {"content-type": "application/json"}
     payload = json.dumps({"name": "Apple AirPods", "data": {"color": "white", "generation": "3rd", "price": 135}})
     response = requests.post(url, data=payload, headers=headers)
     return response, response.json()['id']
 
 
 def update_an_object(obj_id, changed_dict):
-    headers = {'content-type': 'application/json'}
+    headers = {"content-type": "application/json"}
     payload = json.dumps(changed_dict)
-    response = requests.put(f"{url}/{obj_id}", data=payload)
+    response = requests.put(f'{url}/{obj_id}', data=payload, headers=headers)
+    return response
+
+
+def delete_an_object(obj_id):
+    response = requests.delete(f'{url}/{obj_id}')
+    return response
+
+
+def go_to_reqres_auth():
+    data = {"email": "eve.holt@reqres.in", "password": "cityslicka"}
+    return requests.post('https://reqres.in/api/login', json=data)
+
+
+def create_user(token):
+    headers = {"content-type": "application/json",
+               "token": token}
+    return requests.post('https://reqres.in/api/users', json={
+        "name": "morpheus",
+        "jon": "leader"
+    }, headers=headers)
+
+
