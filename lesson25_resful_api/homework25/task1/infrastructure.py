e6d3cad5006fec052aa519e333e8d4000c873839
Mykyta Zhuravlov <zhuravlov.mykyta@gmail.com>
2023-09-20 14:23:02 +0300

    homework25 added

--- lesson25_resful_api/homework25/task1/infrastructure.py
+++ lesson25_resful_api/homework25/task1/infrastructure.py
@@ -15,3 +15,23 @@ def add_object(data):
     headers = {"content-type": "application/json"}
     response = requests.post(url, json=data, headers=headers)
     return response, response.json()['id']
+
+
+def update_an_object(object_id, data):
+    url = f"{base_url}/{object_id}"
+    headers = {"content-type": "application/json"}
+    response = requests.put(url, json=data, headers=headers)
+    return response
+
+
+def patch_an_object(object_id, data):
+    url = f"{base_url}/{object_id}"
+    headers = {"content-type": "application/json"}
+    response = requests.patch(url, json=data, headers=headers)
+    return response
+
+
+def delete_an_object(object_id):
+    url = f"{base_url}/{object_id}"
+    response = requests.delete(url)
+    return response
--- lesson25_resful_api/homework25/task1/test_api.py
+++ lesson25_resful_api/homework25/task1/test_api.py
@@ -25,7 +25,7 @@ def test_get_single_object_2():
     assert "Apple iPhone 12 Mini, 256GB, Blue" in response.text
 
 
-def test_add_object_6():
+def test_add_object():
     data = {
         "name": "Apple AirPods",
         "data": {
@@ -39,3 +39,67 @@ def test_add_object_6():
     assert response.status_code == 200
     assert get_response.status_code == 200
     print(get_response.json())
+
+
+def test_update_an_object():
+    data_to_create = {
+        "name": "Apple MacBook Pro 16",
+        "data": {
+            "year": 2019,
+            "price": 2049.99,
+            "CPU model": "Intel Core i9",
+            "Hard disk size": "1 TB",
+            "color": "silver"
+        }
+    }
+    response, created_object_id = infra.add_object(data_to_create)
+    assert response.status_code == 200
+    data_to_update = {
+        "name": "New Apple MacBook Pro 16",
+        "data": {
+            "year": 2019,
+            "price": 2049.99,
+            "CPU model": "Intel Core i9",
+            "Hard disk size": "1 TB",
+            "color": "silver"
+        }
+    }
+    update_response = infra.update_an_object(created_object_id, data_to_update)
+    assert update_response.status_code == 200
+    print(update_response.json())
+
+
+def test_patch_an_object():
+    data_to_create = {
+        "name": "Apple MacBook Pro 16",
+        "data": {
+            "year": 2019,
+            "price": 2049.99,
+            "CPU model": "Intel Core i9",
+            "Hard disk size": "1 TB",
+            "color": "silver"
+        }
+    }
+    response, created_object_id = infra.add_object(data_to_create)
+    assert response.status_code == 200
+    data_to_patch = {
+        "name": "Apple MacBook Pro 16 (Updated Name)"
+    }
+    patch_response = infra.patch_an_object(created_object_id, data_to_patch)
+    assert patch_response.status_code == 200
+    get_response = infra.get_single_object(created_object_id)
+    updated_data = get_response.json()
+    assert updated_data['name'] == data_to_patch['name']
+    print(updated_data)
+
+
+def test_delete_an_object():
+    data_to_delete = {
+        "name": "Apple to delete"
+    }
+    response, created_object_id = infra.add_object(data_to_delete)
+    assert response.status_code == 200
+    delete_response = infra.delete_an_object(created_object_id)
+    assert delete_response.status_code == 200
+    assert "Object with id" in delete_response.json()["message"]
+    print(delete_response.json())
