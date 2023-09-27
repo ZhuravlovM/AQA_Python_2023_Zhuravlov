1c037f8b4bd66a8caf1fc8fc0a891ce28cf6614d
Mykyta Zhuravlov <zhuravlov.mykyta@gmail.com>
2023-09-27 21:36:14 +0300

    lesson28 updated

--- lesson28_sql/sql_simple_example.py
+++ lesson28_sql/sql_simple_example.py
@@ -1,16 +1,18 @@
 import psycopg2
 
 
-connection = psycopg2.connect(user='***',
-                              password='***',
-                              host='***',
-                              port='***',
-                              database='***')
+connection = psycopg2.connect(user='postgres',
+                              password='1',
+                              host='localhost',
+                              port='5432',
+                              database='postgres')
 
 cursor = connection.cursor()
-cursor.execute('CREATE TABLE testtable (name varchar(25), age int);')
-cursor.execute("INSERT INTO testtable (name, age) VALUES ('ordinary_name', 30), ('unordinary_name', 20);")
-cursor.execute('SELECT * FROM testtable;')
+cursor.execute('CREATE TABLE tablewithusers (id varchar(8) primary key, name varchar(25), last_name varchar(25), email varchar(25), age int);')
+cursor.execute("INSERT INTO tablewithusers (id, name, last_name, email, age) VALUES "
+               "('AAAA', 'ordinary_name', 'ordinary_lastname', 'email@email.com', 30), "
+               "('BBBB', 'ordinary_name', 'ordinary_lastname', 'email@email.com', 30);")
+cursor.execute('SELECT * FROM tablewithusers;')
 connection.commit()
 for row in cursor.fetchall():
     print(row)
