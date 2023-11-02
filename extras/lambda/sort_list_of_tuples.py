7fa7a8ffd98717badc00d6bdf100bc2c5a7e21d0
ZhuravlovM <zhuravlov.mykyta@gmail.com>
2023-11-02 14:39:35 +0200

    pep8 update

--- extras/lambda/sort_list_of_tuples.py
+++ extras/lambda/sort_list_of_tuples.py
@@ -2,5 +2,5 @@
 
 parts_price = [('rim', 50), ('brake pads', 15), ('coolant', 18), ('clips', 4)]
 print("Original:", parts_price)
-parts_price.sort(key= lambda x: x[1])
+parts_price.sort(key=lambda x: x[1])
 print("Sorted:", parts_price)
