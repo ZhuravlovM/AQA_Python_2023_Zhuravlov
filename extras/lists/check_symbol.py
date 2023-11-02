f24749ec5cf3028a7875efd5f492aa6173afe4d8
ZhuravlovM <zhuravlov.mykyta@gmail.com>
2023-11-02 14:30:09 +0200

    lesson26 added

---
+++ extras/lists/check_symbol.py
@@ -0,0 +1,8 @@
+my_list = ['alphabet', 'doorbell', 'axis', 'bootleg', 'zookeeper', 'car']
+new_list = []
+
+for element in my_list:
+    if 'a' in element:
+        new_list.append(element)
+
+print(new_list)
